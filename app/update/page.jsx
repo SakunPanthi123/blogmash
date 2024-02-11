b96f62cb8789a670fb2de64056a6c55e18642d53
SakunPanthi123 <spanthi863@rku.ac.in>
2024-02-11 21:30:55 +0530

    sdfhjgkhlj

--- app/update/page.jsx
+++ app/update/page.jsx
@@ -7,69 +7,13 @@ const Update = () => {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
     const searchParams = useSearchParams()
-    var blogid = 'asdfsd'
-    blogid = searchParams.get('id')
+    const blogid = searchParams.get('id')
 
-    useEffect(() => {
-        const fetchPost = async () => {
-            const response = await fetch(`/api/blog/${blogid}`)
-            const data = await response.json()
-            setTitle(data.title)
-            setContent(data.content)
-        }
-        fetchPost()
-    }, [])
-
-    const handleSubmit = async () => {
-            const response = await fetch(`/api/blog/${blogid}`, {
-            method: "PATCH",
-
-            body: JSON.stringify(
-                {
-                    title: title,
-                    content: content
-                }
-            )
-        })
-        console.log(response)
-        if (response.ok) {
-            router.push('/')
-        }
-    }
-
-    return (
-        <div className='max-w-md mx-auto mt-16'>
-            <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">
-                    Blog Title:
-                </label>
-                <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-            </div>
-            <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="content">
-                    Content:
-                </label>
-                <textarea
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                />
-            </div>
-            <div className="flex items-center justify-center">
-                <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    onClick={handleSubmit}
-                >
-                    Update
-                </button>
-            </div>
-        </div>
-    )
+   return (
+    <div>
+        Update page
+    </div>
+   )
 }
 
 export default Update
